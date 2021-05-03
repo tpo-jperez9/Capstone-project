f3fb82eda36699b55b2bf2ad5c948ffdc5992e99
tpo-jperez9 <jperez9@travelers.com>
2021-05-03 12:05:46 -0500

    updated JP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import apiKey from '../config';
 
 function Home(){
+
+    const [movieData, setMovieData] = useState([]);
+
+    function fetchMovies(){
+        axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+        .then(response => setMovieData(response.data.results))
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, []);
 
     return (
     <header>
---
+++ src/components/MovieData.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+function MovieData(){
+    
+
+    return(
+        <p></p>
+    )
+}
+
+export default MovieData;
